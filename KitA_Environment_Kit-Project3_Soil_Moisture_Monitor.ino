ee2343b61083c01705fc4e009319b8e81c2a4375
STEM IT <127576551+StemITcompany@users.noreply.github.com>
2023-03-16 16:21:53 -0500

    Add files via upload

---
+++ KitA_Environment_Kit-Project3_Soil_Moisture_Monitor.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal_I2C.h>
+
+int sensorPin = A0;
+int soilMoisture = 0;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+lcd.begin();
+lcd.backlight();
+pinMode(sensorPin, INPUT);
+}
+
+void loop() {
+  soilMoisture = analogRead(sensorPin);
+  lcd.print("Soil Moisture: ");
+  lcd.print(soilMoisture);
+  lcd.println("%");
+  delay(1000);
+}
